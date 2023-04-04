5a3b8524cf868dc1ff96275827cdd490c7d9b85e
Alex <alex.ignatov@mail.ru>
2023-04-04 22:21:51 +0300

    Вывести числа от N до 1 с помощью рекурсии

---
+++ Example54/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
+// элементы каждой строки двумерного массива.
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 11);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3}\t");
+        Console.WriteLine();
+    }
+}
+
+void SortArray(int[,] array)
+{
+    int num = 0;
+    for (int k = 0; k < array.GetLength(0); k++)
+        for (int i = 0; i < array.GetLength(0); i++)
+            for (int j = 0; j < array.GetLength(1) - 1; j++)
+            {
+                if (array[i, j] < array[i, j + 1])
+                {
+                    num = array[i, j];
+                    array[i, j] = array[i, j + 1];
+                    array[i, j + 1] = num;
+                }
+            }
+
+}
+
+Console.Write($"Введите количество строк ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Введите количество столбцов ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int n = rows * columns;
+int[,] array = new int[rows, columns];
+
+FillArray(array);
+PrintArray(array);
+
+SortArray(array);
+
+Console.WriteLine($"Отсортированный массив выглядит так:");
+PrintArray(array);
---
+++ Example64/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+//в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+
+string NumbRec(int n)
+    {
+        if(n == 0) return "";
+        return n +" "+ NumbRec(n-1);
+    }
+
+
+
+Console.Write("Введите значение N ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(NumbRec(n));
