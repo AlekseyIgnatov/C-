622c114328e3bc35d3c34abf47e3ce49c1994271
Alex <alex.ignatov@mail.ru>
2023-04-05 11:18:56 +0300

    Вычисление функции Аккермана

--- Example68/Program.cs
+++ Example68/Program.cs
@@ -1,4 +1,18 @@
 ﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
 // Даны два неотрицательных числа m и n.
 
+int Summ(int m, int n)
+    {
+        if(m == n) return m;
+        return m + Summ(m+1, n);
+    }
+
+
+
+Console.Write("Введите значение M ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(Summ(m,n));
 
